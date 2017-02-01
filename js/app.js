cd3fed38f847c460da55b57ff74e89b26325e214
Eric Moore <moore.ericc@gmail.com>
2017-01-31 23:19:58 -0600

    changes

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ var success = function (data) {
 };
 
 $(document).ready(function(){
-  var tweets = twitter.getSearch({'q':'I voted for you','count': 10}, error, success);
+  // var tweets = twitter.getSearch({'q':'I voted for you','count': 10}, error, success);
   // $.ajax({
   //       type: 'GET',
   //       dataType: 'jsonp',
@@ -23,4 +23,24 @@ $(document).ready(function(){
   //           console.log(msg);
   //       }
   //   });
+  //
+  // TODO: make blacklist of words we dont want in tweets - proud, honored, happy
+  // make whitelist of words that make sure its related to trump - trump @realDonaldTrump
+  // convert @realDonaldTrump to "At real donald trump"
+})
+
+$(window).load(function(){
+  setTimeout(function(){
+    var voicelist = responsiveVoice.getVoices();
+    voicelist.forEach(function(voice){
+      console.log(voice.name)
+    })
+    responsiveVoice.speak("And now, huge regrets. By remorseful Trump voters.", "US English Male", { rate: .75, pitch: .75})
+
+    setTimeout(function(){
+      // responsiveVoice.speak(msg, "US English Male", { rate: .75 })
+    }, 1000)
+
+  }, 750)
+
 })
