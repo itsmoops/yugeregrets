1562c10f7f8175cfa5aadc79f5c8767b06c6ae54
Eric Moore <moore.ericc@gmail.com>
2017-02-01 16:08:45 -0600

    testing tweeting waiting wishing

--- src/app.js
+++ src/app.js
@@ -9,26 +9,28 @@ var success = function (data) {
     console.log('Data [%s]', data);
 };
 
-// $(document).ready(function(){
-  // var tweets = twitter.getSearch({'q':'voted for you','count': 10}, error, success);
-  // $.ajax({
-  //       type: 'GET',
-  //       dataType: 'jsonp',
-  //       data: {},
-  //       url: "https://api.twitter.com/1.1/search/tweets.json?q=I%20voted%20for%20you&count=20",
-  //       error: function (jqXHR, textStatus, errorThrown) {
-  //           console.log(jqXHR)
-  //       },
-  //       success: function (msg) {
-  //           console.log(msg);
-  //       }
-  //   });
-  //
-  // TODO: make blacklist of words we dont want in tweets - proud, honored, happy
-  // blacklist if tweet has a photo
-  // make whitelist of words that make sure its related to trump - trump @realDonaldTrump
-  // convert @realDonaldTrump to "At real donald trump"
-// })
+if (typeof document !== "undefined") {
+  $(document).ready(function(){
+    var tweets = twitter.getSearch({'q':'voted for you','count': 10}, error, success);
+    // $.ajax({
+    //       type: 'GET',
+    //       dataType: 'jsonp',
+    //       data: {},
+    //       url: "https://api.twitter.com/1.1/search/tweets.json?q=I%20voted%20for%20you&count=20",
+    //       error: function (jqXHR, textStatus, errorThrown) {
+    //           console.log(jqXHR)
+    //       },
+    //       success: function (msg) {
+    //           console.log(msg);
+    //       }
+    //   });
+    //
+    // TODO: make blacklist of words we dont want in tweets - proud, honored, happy
+    // blacklist if tweet has a photo
+    // make whitelist of words that make sure its related to trump - trump @realDonaldTrump
+    // convert @realDonaldTrump to "At real donald trump"
+  })
+}
 
 if (typeof window !== "undefined") {
   $(window).load(function(){
--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
       historyApiFallback: true,
       watchOptions: { aggregateTimeout: 300, poll: 1000 },
       headers: {
-        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Origin": "https://yugeregrets.herokuapp.com/",
         "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
         "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
       }
@@ -26,12 +26,12 @@ module.exports = {
             jQuery: "jquery",
             "window.jQuery": "jquery"
         }),
-        new webpack.optimize.DedupePlugin(),
-        new webpack.optimize.UglifyJsPlugin({
-             minimize: true,
-             compress: {
-               warnings: false
-             }
-            })
+        new webpack.optimize.DedupePlugin(https://yugeregrets.herokuapp.com/
+        // new webpack.optimize.UglifyJsPlugin({
+        //      minimize: true,
+        //      compress: {
+        //        warnings: false
+        //      }
+        //     })
     ]
 };
