42f1450fde054064e71c90cfafba50eb8eba558c
Eric Moore <moore.ericc@gmail.com>
2017-01-31 20:08:01 -0600

    testing bundle code

---
+++ data/twitter_config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    "consumerKey": "fD4GF1zWhdmgxrRnRSi3R4Tfo",
+    "consumerSecret": "CILjXY3h2o16OzNNAqqlLja0jx1Ps4nfkhJFhH5y24d2Xfbfqd",
+    "accessToken": "862420878-dbz0dyCPmLB96camnR8IujA00LH61uaj8RNCIBVO",
+    "accessTokenSecret": "qAXXiiLxehwI6R3DgK1SvGqmzDbz9HWd2hm0orYMzQVLn",
+    "callBackUrl": "http://127.0.0.1:8080/"
+}
--- js/app.js
+++ js/app.js
@@ -1,5 +1,15 @@
+var config = require('../data/twitter_config')
 var Twitter = require('twitter-node-client').Twitter;
-debugger
+var twitter = new Twitter(config);
+
+var error = function (err, response, body) {
+    console.log('ERROR [%s]', err);
+};
+var success = function (data) {
+    console.log('Data [%s]', data);
+};
+
 $(document).ready(function(){
+  var tweets = twitter.getSearch({'q':'I voted for you','count': 10}, error, success);
   // $('.title-container').fadeIn("slow")
 })
