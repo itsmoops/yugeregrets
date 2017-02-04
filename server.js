d987431b5bfab1bb36cea22c78333aa22417639b
Eric Moore <moore.ericc@gmail.com>
2017-02-04 13:28:36 -0600

    server now grabs tweets from @Trump_Regrets - convert video to mp4, add loop able audio files

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var moment = require('moment')
 var jsonfile = require('jsonfile')
 var fs = require('fs')
 
-var error = function (err, response, body) {
+var error = (err, response, body) => {
     console.log('ERROR [%s]', err);
 };
 
@@ -17,20 +17,20 @@ app.set('json spaces', 2)
 
 app.use(express.static(__dirname + '/src'))
 
-app.get('/', function(request, response) {
+app.get('/', (request, response) => {
   response.render('pages/index')
 })
 
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), () => {
   console.log('Node app is running on port', app.get('port'))
 })
 
 /**
 	* checks to see if a tweets.json file already exists
 */
-function _fileExists(filePath) {
-    return new Promise(function(resolve, reject) {
-        fs.stat(filePath, function(err, stat) {
+_fileExists = (filePath) => {
+    return new Promise((resolve, reject) => {
+        fs.stat(filePath, (err, stat) => {
             if (err == null) {
                  resolve(true)
             } else if (err && err.code == 'ENOENT') {
@@ -40,13 +40,18 @@ function _fileExists(filePath) {
     })
 }
 
-function _getTweets(filePath) {
-  return new Promise(function(resolve, reject) {
-    twitter.getSearch({'q':'@realdonaldtrump "voted for you"', 'count': 100, 'tweet_mode': 'extended'}, error, function(data) {
+_getTweets = (filePath) => {
+  return new Promise((resolve, reject) => {
+    var searchParams = {
+      screen_name: 'Trump_Regrets',
+      count: '50',
+      tweet_mode: 'extended'
+    }
+    twitter.getUserTimeline(searchParams, error, (data) => {
       data = JSON.parse(data)
       data.DATE_GENERATED = moment()
       var spaces = {spaces: 2};
-      jsonfile.writeFile(filePath, data, spaces, function(err) {
+      jsonfile.writeFile(filePath, data, spaces, (err) => {
           if (err) {
               console.log(err)
           }
@@ -56,18 +61,18 @@ function _getTweets(filePath) {
   })
 }
 
-app.get('/tweets', function(request, response) {
+app.get('/tweets', (request, response) => {
   var filePath = './data/tweets.json'
   var tweets = {}
-  _fileExists(filePath).then(function(exists) {
+  _fileExists(filePath).then((exists) => {
       if (exists) {
-        var today = moment();
+        var today = moment()
         tweets = require(filePath)
         /* we only want to generate new tweets once per day
         * so we check against the "DATE_GENERATED" property */
         if (today.isAfter(tweets.DATE_GENERATED, 'day')) {
             // if it's been more than a day, regenerate tweets
-            _getTweets(filePath).then(function(data) {
+            _getTweets(filePath).then((data) => {
               tweets = data
               response.json(tweets)
             })
@@ -77,7 +82,7 @@ app.get('/tweets', function(request, response) {
         }
       } else {
         // generate tweets for the first time if file doesn't exist
-        _getTweets(filePath).then(function(data){
+        _getTweets(filePath).then((data) => {
           tweets = data
           response.json(tweets)
         })
