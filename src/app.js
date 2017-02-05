44a6fa3e24b7863df3319ed54cf65d31a81889b5
Michael Grenier <michael.grenier@bestbuy.com>
2017-02-05 12:12:21 -0600

    Fix volume buttons

--- src/app.js
+++ src/app.js
@@ -136,9 +136,8 @@ class Intro extends Component {
 }
 
 const MuteButton = ({ enabled, onClick, children }) => (
-  <button className="controls__button" type="button" onClick={ () => onClick(!enabled) }>
+  <button className={ `controls__button controls__button--${enabled ? 'enabled' : 'disabled'}` } type="button" onClick={ () => onClick(!enabled) }>
     { children }
-    { enabled ? '🔊' : '🔇' }
   </button>
 )
 
@@ -148,8 +147,17 @@ class Controls extends Component {
 
     return (
       <div className="controls">
-        <MuteButton enabled={ music } onClick={ onMuteMusic }> Music </MuteButton>
-        <MuteButton enabled={ speech } onClick={ onMuteSpeech }> Speech </MuteButton>
+        <MuteButton enabled={ music } onClick={ onMuteMusic }>
+          <svg version="1.1" width="32" height="32" viewBox="0 0 32 32" className="controls__icon">
+            <path d="M30 0h2v23c0 2.761-3.134 5-7 5s-7-2.239-7-5c0-2.761 3.134-5 7-5 1.959 0 3.729 0.575 5 1.501v-11.501l-16 3.556v15.444c0 2.761-3.134 5-7 5s-7-2.239-7-5c0-2.761 3.134-5 7-5 1.959 0 3.729 0.575 5 1.501v-19.501l18-4z"></path>
+          </svg>
+        </MuteButton>
+        <MuteButton enabled={ speech } onClick={ onMuteSpeech }>
+          <svg version="1.1" width="32" height="32" viewBox="0 0 32 32" className="controls__icon">
+            <path d="M22.485 25.985c-0.384 0-0.768-0.146-1.061-0.439-0.586-0.586-0.586-1.535 0-2.121 4.094-4.094 4.094-10.755 0-14.849-0.586-0.586-0.586-1.536 0-2.121s1.536-0.586 2.121 0c2.55 2.55 3.954 5.94 3.954 9.546s-1.404 6.996-3.954 9.546c-0.293 0.293-0.677 0.439-1.061 0.439v0zM17.157 23.157c-0.384 0-0.768-0.146-1.061-0.439-0.586-0.586-0.586-1.535 0-2.121 2.534-2.534 2.534-6.658 0-9.192-0.586-0.586-0.586-1.536 0-2.121s1.535-0.586 2.121 0c3.704 3.704 3.704 9.731 0 13.435-0.293 0.293-0.677 0.439-1.061 0.439z"></path>
+            <path d="M13 30c-0.26 0-0.516-0.102-0.707-0.293l-7.707-7.707h-3.586c-0.552 0-1-0.448-1-1v-10c0-0.552 0.448-1 1-1h3.586l7.707-7.707c0.286-0.286 0.716-0.372 1.090-0.217s0.617 0.519 0.617 0.924v26c0 0.404-0.244 0.769-0.617 0.924-0.124 0.051-0.254 0.076-0.383 0.076z"></path>
+          </svg>
+        </MuteButton>
       </div>
     )
   }
@@ -172,8 +180,12 @@ class Main extends Component {
       delay(10000)
     ])
       .then(([ tweets ]) => tweets.reduce((prom, tweet, index) => prom.then(() => {
+        const { speech } = this.state
+
         this.setState({ tweet, index })
-        if (this.state.speech) speak(tweet.full_text)
+
+        if (speech) speak(tweet.full_text)
+
         return delay(11000) // scroll transition minus fade transition
       }), Promise.resolve()))
       .then(() => loop.on('end', () => {
